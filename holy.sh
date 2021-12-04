a676c00ecfe48c072f531d15ab576e3f360a0521
shaquibimdad <shaquibxyz@gmail.com>
2021-12-04 20:42:02 +0530

    Some changes

---
+++ holy.sh
@@ -0,0 +1,7 @@
+ln -s ~/mynixconfigs/.config/bspwm/ ~/.config/bspwm
+ln -s ~/mynixconfigs/.config/polybar/ ~/.config/polybar
+ln -s ~/mynixconfigs/.config/sxhkd/ ~/.config/sxhkd
+ln -s ~/mynixconfigs/.config/plank/ ~/.config/plank
+ln -s ~/mynixconfigs/.config/gtk-2.0/ ~/.config/gtk-2.0
+ln -s ~/mynixconfigs/.config/gtk-3.0/ ~/.config/gtk-3.0
+echo "Done"
