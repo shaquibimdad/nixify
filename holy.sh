f42124c1fd266529dd6390de26359a318ce557ab
shaquibimdad <shaquibxyz@gmail.com>
2021-12-12 22:35:25 +0530

    Initial alacritty configs

--- holy.sh
+++ holy.sh
@@ -4,4 +4,6 @@ ln -s ~/mynixconfigs/.config/sxhkd/ ~/.config/sxhkd
 ln -s ~/mynixconfigs/.config/plank/ ~/.config/plank
 ln -s ~/mynixconfigs/.config/gtk-2.0/ ~/.config/gtk-2.0
 ln -s ~/mynixconfigs/.config/gtk-3.0/ ~/.config/gtk-3.0
+ln -s ~/mynixconfigs/.config/rofi/ ~/.config/rofi
+ln -s ~/mynixconfigs/.config/alacritty ~/.config/alacritty
 echo "Done"
