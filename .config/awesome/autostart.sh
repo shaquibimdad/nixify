b11959478ce3facf507eed572aa8db47ed709e1a
shaquibimdad <shaquibxyz@gmail.com>
2021-12-19 16:26:04 +0530

    WM: Switched to Awesome with polybar

---
+++ .config/awesome/autostart.sh
@@ -0,0 +1,7 @@
+$HOME/.config/polybar/launch.sh
+picom --config $HOME/.config/bspwm/picom.conf &
+feh --bg-fill $HOME/mynixconfigs/.config/bspwm/wall/holy.jpg
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+nm-applet &
+plank &
+flameshot &
