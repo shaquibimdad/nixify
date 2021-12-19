9634355a1be20ffb0af9769c288ca711ae8a3fa3
shaquibimdad <shaquibxyz@gmail.com>
2021-12-19 18:38:06 +0530

    Cleanup

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -1,6 +1,6 @@
-$HOME/.config/polybar/launch.sh
-picom --config $HOME/.config/bspwm/picom.conf &
-feh --bg-fill $HOME/mynixconfigs/.config/bspwm/wall/holy.jpg
+$HOME/mynixconfigs/.config/polybar/launch.sh
+picom --config $HOME/mynixconfigs/.config/awesome/picom.conf &
+feh --bg-fill $HOME/mynixconfigs/.config/awesome/wall/holy.jpg
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 nm-applet &
 plank &
