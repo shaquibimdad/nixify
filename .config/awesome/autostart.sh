710a1826bd2ddbb2b0717ded9cf71ab5c5308911
shaquibimdad <shaquibxyz@gmail.com>
2021-12-23 21:43:00 +0530

    Delete: Awesome,Polybar,Sxhkd configs                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       Switched to xfwm.

--- .config/awesome/autostart.sh
+++
@@ -1,7 +0,0 @@
-#$HOME/mynixconfigs/.config/polybar/launch.sh
-picom --config $HOME/mynixconfigs/.config/awesome/picom.conf &
-feh --bg-fill $HOME/mynixconfigs/.config/awesome/wall/holy.jpg
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-nm-applet &
-#plank &
-flameshot &
