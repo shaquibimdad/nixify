da9df28cf6139f2d50d55328156c3df735646616
shaquibimdad <shaquibxyz@gmail.com>
2021-12-23 21:43:00 +0530

    Delete: Awesome,Polybar,Sxhkd configs                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       Switched to xfwm.

--- holy.sh
+++ holy.sh
@@ -7,4 +7,5 @@ ln -s ~/mynixconfigs/.config/gtk-3.0/ ~/.config/gtk-3.0
 ln -s ~/mynixconfigs/.config/rofi/ ~/.config/rofi
 ln -s ~/mynixconfigs/.config/alacritty ~/.config/alacritty
 ln -s ~/mynixconfigs/.config/awesome ~/.config/awesome
+ln -s ~/mynixconfigs/.config/picom ~/.config/picom
 echo "Done"
